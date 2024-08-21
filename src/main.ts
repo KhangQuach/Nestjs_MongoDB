d49f831a558a48783978d10cedca169906f8a10c
Khang Quách <khangB2203505@student.ctu.edu.vn>
2024-08-22 01:08:19 +0700

    BaseProject

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const port = configService.get<number>('PORT') || 8080;
+  const port = configService.get('PORT');
+
   await app.listen(port);
 }
 bootstrap();
